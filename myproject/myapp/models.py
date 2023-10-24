2f20ee4f6ed24afb0610c05fe6f2e177ba5b0a10
joaodleandro <joaodavidleandro00@gmail.com>
2023-10-24 09:33:37 +0100

    Configuration Files
    Initial setup for django and its components.

---
+++ myproject/myapp/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Photo(models.Model):
+    image = models.ImageField(null=False, blank=False, upload_to="images/")
---
+++ myproject/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
+
+# Create your views here.
+def index(request):
+    pass
