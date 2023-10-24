3999c16f0ade562876b632eba14bfe8034d4d819
joaodleandro <joaodavidleandro00@gmail.com>
2023-10-24 10:11:51 +0100

    Image adds to database.
    Image is uploaded to database, working now on display of them in index.html.

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), #root url
+    path('upload', views.upload_image, name='upload'),
+    # path('details', views.details, name='details'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+
+from .forms import ImageUploadForm
 from .models import Photo
 
 # Create your views here.
 def index(request):
     pass
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            Photo = form.save()
+            Photo.save()
+            return redirect('/')  
+    else:
+        form = ImageUploadForm()
+        return render(request, 'upload.html', {'form': form}) 
