bf26a924beb9256053a36457b5c732890a6378c7
joaodleandro <joaodavidleandro00@gmail.com>
2023-10-24 13:31:33 +0100

    CRUD actions
    Fully implementation of the rest of CRUD actions such as update and view with some minor alterations in html templates.

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'), #root url
     path('upload', views.upload_image, name='upload'),
     path('confirm_delete/<int:pk>', views.confirm_delete, name='confirm_delete'),
-    # path('details', views.details, name='details'),
+    path('view_image/<int:pk>', views.view_image, name='view_image'), 
+    path('update_image/<int:pk>', views.update_image, name='update_image'), 
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -9,9 +9,7 @@ from .models import Photo
 
 # Create your views here.
 def index(request):
-
     gallery = Photo.objects.all()
-
     return render(request, 'index.html', {'gallery': gallery}) 
 
 def upload_image(request):
@@ -34,3 +32,22 @@ def confirm_delete(request, pk):
         return redirect('/')
     else:
         return render(request, 'confirm_delete.html', {'pk': pk})
+
+def view_image(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'view_image.html', {'photo': photo})
+
+def update_image(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES, instance=photo)
+        default_storage.delete(photo.image.path)
+        if form.is_valid():
+            form.save()
+            return redirect('view_image', pk=photo.pk) 
+    else:
+        form = ImageUploadForm(instance=photo)
+        return render(request, 'update_image.html', {'form': form, 'photo': photo})
+    
+    
