50362c629a73f4d3e19f21a7016044ffe829aca6
joaodleandro <joaodavidleandro00@gmail.com>
2023-10-24 09:38:47 +0100

    Quick fix
    Quick fix on wrong model name in various files.

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Feature
+from .models import Photo
 
 # Create your views here.
 def index(request):
